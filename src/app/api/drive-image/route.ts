5ed4df50a442581ad88f60a19f649f8e0a9caa14
sayyidtrq <sayyidthariq02@gmail.com>
2025-12-05 20:23:02 +0700

    trigger deployment

--- src/app/api/drive-image/route.ts
+++ src/app/api/drive-image/route.ts
@@ -25,7 +25,7 @@ export async function GET(request: NextRequest) {
       );
     }
 
-    // Initialize OAuth2 client with your company's credentials
+    // Initialize OAuth2 client with your company's credentialssss
     const oauth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_CLIENT_SECRET,
