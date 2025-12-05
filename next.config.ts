d9da6bff6ba6fd44bb19a87ccbc91282e8671183
trias.fahri <trias.fahri@ui.ac.id>
2025-12-05 20:03:56 +0700

    fixed fetching drive image + added caching

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,30 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rajawalilelangindo.com',
+        pathname: '/api/drive-image',
+      },
+      {
+        protocol: 'https',
+        hostname: 'rajawali-lelang-web.vercel.app',
+        pathname: '/api/drive-image',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/drive-image',
+      },
+    ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 31536000,
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
+    contentDispositionType: 'inline',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   experimental: {
